97a9d6fd952a93ad9f6324f80194aa60967bbd30
malhandha <malfianhandiy@gmail.com>
2025-03-18 10:30:56 +0700

    dasdsasa

---
+++ db.php
@@ -0,0 +1,22 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "VillWork";
+$koneksi = mysqli_connect($host, $user, $password, $database);
+if (!$koneksi) {
+die("Koneksi gagal: " . mysqli_connect_error());
+}
+?>
+<?php
+try {
+    $dsn = "mysql:host=localhost;dbname=kampus";
+    $username = "root";
+    $password = "";
+    $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO($dsn, $username, $password, $options);
+    echo "Koneksi berhasil!";
+    } catch (PDOException $e) {
+    echo "Koneksi gagal: " . $e->getMessage();
+    }
+    ?>
