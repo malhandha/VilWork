743465b087da5355c2ac3c3fbfb2d37a80081d20
malhandha <malfianhandiy@gmail.com>
2025-03-23 13:43:31 +0700

    sad

--- db.php
+++ db.php
@@ -1,22 +1,15 @@
+<form action="db.php" method="POST">
+Nama: <input type="text" name="nama"><br>
+Email : <input type="text" name = "email"><br>
+password : <input type="text" name="password">
+<input type="submit" name="submit" value="Tambah Data">
+</form>
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "VillWork";
-$koneksi = mysqli_connect($host, $user, $password, $database);
-if (!$koneksi) {
-die("Koneksi gagal: " . mysqli_connect_error());
+include 'koneksi.php';
+if(isset($_POST['submit'])){
+$nama = $_POST['nama'];;
+$query = "INSERT INTO users (nama) VALUES ('$nama')";
+mysqli_query ($koneksi, $query);
+echo "Data berhasil ditambahkan.";
 }
 ?>
-<?php
-try {
-    $dsn = "mysql:host=localhost;dbname=kampus";
-    $username = "root";
-    $password = "";
-    $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
-    $pdo = new PDO($dsn, $username, $password, $options);
-    echo "Koneksi berhasil!";
-    } catch (PDOException $e) {
-    echo "Koneksi gagal: " . $e->getMessage();
-    }
-    ?>
