3bfd7e60832df774ccd40f9d821a5672c39ebc3e
malhandha <malfianhandiy@gmail.com>
2025-04-16 17:53:07 +0700

    update header

---
+++ dashboard.php
@@ -0,0 +1,789 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="style.css">
+    <title>Responsive NFT Website</title>
+</head>
+
+<body>
+
+    <nav>
+        <a href="#" class="logo">AsmrProg</a>
+        <div class="links">
+            <a href="#">Home</a>
+            <a href="#">Features</a>
+            <a href="#">Blog</a>
+            <a href="#">About Us</a>
+        </div>
+        <div class="login">
+            <button class="signup">Get Started</button>
+            <button>Login</button>
+        </div>
+    </nav>
+
+    <div class="konten_utama">
+        <div class="left">
+            <h1>Let's Buy Some <span>AsmrGraph</span></h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, eaque harum. Nobis ipsam magni labore?
+            </p>
+            <a href="#">
+                <i class='bx bx-basket'></i>
+                <span>Buy Now</span>
+            </a>
+        </div>
+        <img src="assets/header.png">
+    </div>
+
+    <h2 class="separator">
+        Sell Your NFTs
+    </h2>
+
+    <div class="sell-nft">
+        <div class="item">
+            <div class="konten_utama">
+                <i class='bx bx-wallet-alt'></i>
+                <h5>Connect Wallet</h5>
+            </div>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste modi ab sunt dolores ex dolor, suscipit
+                fuga sed earum.</p>
+        </div>
+        <div class="item">
+            <div class="konten_utama">
+                <i class='bx bx-cart-alt'></i>
+                <h5>NFT Marketplace</h5>
+            </div>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste modi ab sunt dolores ex dolor, suscipit
+                fuga sed earum.</p>
+        </div>
+        <div class="item">
+            <div class="konten_utama">
+                <i class='bx bx-grid-alt'></i>
+                <h5>Launch DApps</h5>
+            </div>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste modi ab sunt dolores ex dolor, suscipit
+                fuga sed earum.</p>
+        </div>
+    </div>
+
+    <h2 class="separator">
+        Super Hot NFTs
+    </h2>
+
+    <div class="nft-shop">
+        <div class="category">
+            <a href="#">AsmrGraph</a>
+            <a href="#">Abstract</a>
+            <a href="#">Monkey</a>
+            <a href="#">Cars</a>
+            <a href="#">Trending</a>
+        </div>
+
+        <div class="nft-list">
+            <div class="item">
+                <img src="assets/item-1.png">
+                <div class="info">
+                    <div>
+                        <h5>CryptoArt</h5>
+                        <div class="btc">
+                            <i class='bx bxl-bitcoin'></i>
+                            <p>0.29 BTC</p>
+                        </div>
+                    </div>
+                    <p>5 of 33</p>
+                </div>
+                <div class="bid">
+                    <p>2h 28m 1s</p>
+                    <a href="#">Place a Bid</a>
+                </div>
+            </div>
+            <div class="item">
+                <img src="assets/item-2.png">
+                <div class="info">
+                    <div>
+                        <h5>Abs-Art</h5>
+                        <div class="btc">
+                            <i class='bx bxl-bitcoin'></i>
+                            <p>0.21 BTC</p>
+                        </div>
+                    </div>
+                    <p>7 of 12</p>
+                </div>
+                <div class="bid">
+                    <p>1h 28m 1s</p>
+                    <a href="#">Place a Bid</a>
+                </div>
+            </div>
+            <div class="item">
+                <img src="assets/item-3.png">
+                <div class="info">
+                    <div>
+                        <h5>TestArt</h5>
+                        <div class="btc">
+                            <i class='bx bxl-bitcoin'></i>
+                            <p>0.21 BTC</p>
+                        </div>
+                    </div>
+                    <p>9 of 23</p>
+                </div>
+                <div class="bid">
+                    <p>4h 21m 1s</p>
+                    <a href="#">Place a Bid</a>
+                </div>
+            </div>
+            <div class="item">
+                <img src="assets/item-4.png">
+                <div class="info">
+                    <div>
+                        <h5>Art-4</h5>
+                        <div class="btc">
+                            <i class='bx bxl-bitcoin'></i>
+                            <p>0.45 BTC</p>
+                        </div>
+                    </div>
+                    <p>1 of 33</p>
+                </div>
+                <div class="bid">
+                    <p>0h 28m 1s</p>
+                    <a href="#">Place a Bid</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="view-more">
+        <button>View More</button>
+    </div>
+
+    <h2 class="separator">
+        Best Sellers
+    </h2>
+
+    <div class="sellers">
+        <div class="item">
+            <img src="assets/profile-1.png">
+            <div class="info">
+                <h4>Jhone D</h4>
+                <p>15.2K</p>
+            </div>
+        </div>
+        <div class="item">
+            <img src="assets/profile-2.png">
+            <div class="info">
+                <h4>Gregory B</h4>
+                <p>12.7K</p>
+            </div>
+        </div>
+        <div class="item">
+            <img src="assets/profile-3.png">
+            <div class="info">
+                <h4>Stephen E</h4>
+                <p>7.9K</p>
+            </div>
+        </div>
+        <div class="item">
+            <img src="assets/profile-4.png">
+            <div class="info">
+                <h4>Russell M</h4>
+                <p>4.2K</p>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <h3>Create, Explore & Collect Abstract NFTs</h3>
+        <div class="right">
+            <div class="links">
+                <a href="#">Privacy Policy</a>
+                <a href="#">Cooperation</a>
+                <a href="#">Sponsorship</a>
+                <a href="#">Contact Us</a>
+            </div>
+            <div class="social">
+                <i class='bx bxl-instagram'></i>
+                <i class='bx bxl-facebook-square'></i>
+                <i class='bx bxl-github'></i>
+            </div>
+            <p>Copyright © 2024 AsmrProg, All Rights Reserved.</p>
+        </div>
+    </footer>
+
+</body>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap');
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Ubuntu', sans-serif;
+    }
+
+    a {
+        text-decoration: none;
+        color: #000;
+    }
+
+    img {
+        object-fit: cover;
+    }
+
+    body {
+        background-color: #f1f1f1;
+    }
+
+    nav {
+        padding: 0 80px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        height: 80px;
+    }
+
+    nav a.logo {
+        font-size: 33px;
+        color: #0f2182;
+        font-weight: 600;
+    }
+
+    nav .links a:not(:last-child) {
+        margin-right: 20px;
+    }
+
+    nav .login button {
+        padding: 8px 14px;
+        border: none;
+        cursor: pointer;
+        background-color: transparent;
+    }
+
+    nav .login button.signup {
+        background-color: #000;
+        color: #fff;
+        border-radius: 4px;
+        margin-right: 14px;
+    }
+
+    .konten_utama {
+        padding: 0 80px;
+        height: calc(100vh - 80px);
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .konten_utama .left {
+        width: 700px;
+    }
+
+    .konten_utama .left h1 {
+        font-size: 80px;
+    }
+
+    .konten_utama .left h1 span {
+        color: #0f2182;
+    }
+
+    .konten_utama .left p {
+        margin: 40px 0;
+        color: #777;
+    }
+
+    .konten_utama .left a {
+        display: flex;
+        align-items: center;
+        background: #000;
+        width: 200px;
+        padding: 8px;
+        border-radius: 60px;
+    }
+
+    .konten_utama .left a i {
+        background-color: #fff;
+        font-size: 24px;
+        border-radius: 50%;
+        padding: 8px;
+    }
+
+    .konten_utama .left a span {
+        color: #fff;
+        margin-left: 10px;
+    }
+
+    .konten_utama img {
+        width: 600px;
+    }
+
+    h2.separator {
+        padding: 0 80px;
+        font-size: 40px;
+        margin-top: 40px;
+    }
+
+    .sell-nft {
+        padding: 0 80px;
+        margin: 50px 0 80px;
+        display: flex;
+        justify-content: space-between;
+        gap: 30px;
+    }
+
+    .sell-nft .item,
+    .nft-shop .nft-list .item {
+        padding: 20px;
+        background: #fff;
+        border-radius: 18px;
+        box-shadow: 0 10px 30px rgba(0, 0, 0, 0.1);
+        transition: all 0.3s ease;
+    }
+
+    .sell-nft .item:hover,
+    .nft-shop .nft-list .item:hover,
+    .sellers .item:hover {
+        box-shadow: none;
+    }
+
+    .sell-nft .item .konten_utama {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+    }
+
+    .sell-nft .item .konten_utama i {
+        font-size: 40px;
+    }
+
+    .sell-nft .item .konten_utama h5 {
+        font-size: 17px;
+    }
+
+    .sell-nft .item p {
+        margin-top: 10px;
+        padding: 0 4px;
+    }
+
+    .nft-shop {
+        padding: 0 80px;
+        margin-top: 30px;
+    }
+
+    .nft-shop .category {
+        display: flex;
+        gap: 14px;
+    }
+
+    .nft-shop .category a {
+        background-color: #dfdcdc;
+        padding: 8px 14px;
+        font-size: 15px;
+        border-radius: 14px;
+        transition: all 0.3s ease;
+    }
+
+    .nft-shop .category a:hover {
+        color: #fff;
+        background-color: #0f2182;
+    }
+
+    .nft-shop .nft-list {
+        padding: 40px 0;
+        display: flex;
+        justify-content: space-between;
+        gap: 20px;
+    }
+
+    .nft-shop .nft-list .item img {
+        width: 280px;
+        height: 280px;
+        border-radius: 18px;
+    }
+
+    .nft-shop .nft-list .item .info {
+        display: flex;
+        align-items: flex-end;
+        justify-content: space-between;
+        padding: 12px 0;
+        border-bottom: 1px solid #edecec;
+    }
+
+    .nft-shop .nft-list .item .info h5 {
+        font-size: 18px;
+    }
+
+    .nft-shop .nft-list .item .info .btc {
+        margin-top: 10px;
+        display: flex;
+        align-items: center;
+        gap: 5px;
+    }
+
+    .nft-shop .nft-list .item .btc i {
+        font-size: 22px;
+        color: #304ffe;
+    }
+
+    .nft-shop .nft-list .item .btc p {
+        font-size: 12px;
+        color: #304ffe;
+    }
+
+    .nft-shop .nft-list .item .info>p {
+        font-size: 12px;
+        color: #989898;
+    }
+
+    .nft-shop .nft-list .item .bid {
+        padding: 14px 0 4px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .nft-shop .nft-list .item .bid p,
+    .nft-shop .nft-list .item .bid a {
+        background-color: #edecec;
+        padding: 6px 10px;
+        font-size: 13px;
+        border-radius: 10px;
+    }
+
+    .nft-shop .nft-list .item .bid a {
+        color: #304ffe;
+        background: transparent;
+        font-weight: bold;
+        transition: all 0.3s ease;
+    }
+
+    .nft-shop .nft-list .item .bid a:hover {
+        color: #fff;
+        background-color: #000;
+    }
+
+    .view-more {
+        margin: 10px 0 40px;
+        display: flex;
+        justify-content: center;
+    }
+
+    .view-more button {
+        padding: 10px 20px;
+        font-size: 16px;
+        border: none;
+        background: #dfdcdc;
+        border-radius: 10px;
+        cursor: pointer;
+        transition: all 0.3s ease;
+    }
+
+    .view-more button:hover {
+        color: #fff;
+        background: #0f2182;
+    }
+
+    .sellers {
+        padding: 0 80px;
+        margin: 40px 0 80px;
+        display: flex;
+        gap: 30px;
+    }
+
+    .sellers .item {
+        background: #fff;
+        display: flex;
+        align-items: center;
+        width: 25%;
+        gap: 14px;
+        padding: 14px;
+        border-radius: 18px;
+        cursor: pointer;
+        box-shadow: 0 10px 30px rgba(0, 0, 0, 0.1);
+        transition: all 0.3s ease;
+    }
+
+    .sellers .item img {
+        width: 64px;
+    }
+
+    .sellers .item .info p {
+        font-size: 14px;
+        margin-top: 8px;
+    }
+
+    footer {
+        background-color: #000;
+        color: #fff;
+        padding: 100px 80px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    footer h3 {
+        font-size: 48px;
+    }
+
+    footer .right {
+        width: 80%;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        gap: 26px;
+    }
+
+    footer .right .links {
+        display: flex;
+        gap: 30px;
+    }
+
+    footer .right .links a {
+        color: #989898;
+        transition: color 0.3s ease;
+    }
+
+    footer .right .links a:hover {
+        color: #fff;
+    }
+
+    footer .right .social {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+    }
+
+    footer .right .social i {
+        font-size: 30px;
+        cursor: pointer;
+        transition: color 0.3s ease;
+    }
+
+    footer .right .social i:hover {
+        color: #304ffe;
+    }
+
+    footer .right p {
+        font-size: 13px;
+        color: #777;
+    }
+
+    @media screen and (max-width: 1400px) {
+
+        nav,
+        .konten_utama,
+        h2.separator,
+        .sell-nft,
+        .nft-shop,
+        .sellers {
+            padding: 0 40px;
+        }
+
+        .konten_utama .left h1 {
+            font-size: 60px;
+        }
+
+        .konten_utama img {
+            width: 540px;
+        }
+
+        .nft-shop .nft-list .item img {
+            width: 220px;
+            height: 220px;
+        }
+
+        footer {
+            padding: 100px 40px;
+        }
+
+        footer h3 {
+            font-size: 38px;
+        }
+
+    }
+
+    @media screen and (max-width: 1200px) {
+
+        .konten_utama .left h1 {
+            font-size: 48px;
+        }
+
+        .konten_utama img {
+            width: 480px;
+        }
+
+        h2.separator {
+            font-size: 34px;
+        }
+
+        .sell-nft .item .konten_utama i {
+            font-size: 36px;
+        }
+
+        .sell-nft .item .konten_utama h5 {
+            font-size: 15px;
+        }
+
+        .sell-nft .item p {
+            font-size: 13px;
+        }
+
+        .nft-shop .nft-list {
+            flex-wrap: wrap;
+            row-gap: 30px;
+        }
+
+        .nft-shop .nft-list .item {
+            width: 48%;
+        }
+
+        .nft-shop .nft-list .item img {
+            width: 100%;
+            height: auto;
+        }
+
+        .sellers {
+            flex-wrap: wrap;
+        }
+
+        .sellers .item {
+            width: 48%;
+        }
+
+        footer .links a {
+            font-size: 15px;
+        }
+
+    }
+
+    @media screen and (max-width: 992px) {
+
+        nav a.logo {
+            font-size: 28px;
+        }
+
+        nav .links a {
+            font-size: 14px;
+        }
+
+        .konten_utama {
+            flex-direction: column-reverse;
+            height: auto;
+            margin-bottom: 60px;
+        }
+
+        .konten_utama .left {
+            width: 100%;
+        }
+
+        .konten_utama img {
+            width: 100%;
+        }
+
+        .sell-nft {
+            flex-wrap: wrap;
+            gap: 20px;
+        }
+
+        .sell-nft .item {
+            width: 48%;
+        }
+
+        .sellers {
+            gap: 20px;
+        }
+
+        footer {
+            flex-direction: column;
+            padding: 60px 40px;
+        }
+
+        footer h3 {
+            margin-bottom: 40px;
+            font-size: 32px;
+        }
+
+        footer .right {
+            width: 100%;
+            align-items: center;
+        }
+
+    }
+
+    @media screen and (max-width: 768px) {
+
+        nav,
+        .konten_utama,
+        h2.separator,
+        .sell-nft,
+        .nft-shop,
+        .sellers {
+            padding: 0 20px;
+        }
+
+        nav a.logo {
+            display: none;
+        }
+
+        nav .links a {
+            font-size: 12px;
+        }
+
+        nav .login button {
+            font-size: 12px;
+            padding: 6px 10px;
+        }
+
+        .konten_utama .left h1 {
+            font-size: 36px;
+        }
+
+        .konten_utama .left p {
+            font-size: 14px;
+        }
+
+        .konten_utama .left a {
+            padding: 6px;
+            width: 160px;
+            font-size: 14px;
+        }
+
+        .konten_utama .left a i {
+            padding: 6px;
+        }
+
+        h2.separator {
+            font-size: 30px;
+        }
+
+        footer {
+            padding: 40px 20px;
+        }
+
+    }
+
+    @media screen and (max-width: 576px) {
+
+        .konten_utama .left h1 {
+            font-size: 30px;
+        }
+
+        .sell-nft .item {
+            width: 100%;
+        }
+
+        .nft-shop .nft-list .item,
+        .sellers .item {
+            width: 47%;
+        }
+
+        .nft-shop .category a {
+            font-size: 13px;
+        }
+
+    }
+</style>
+
+</html>
